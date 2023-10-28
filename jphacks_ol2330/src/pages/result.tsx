a77afccb4e12d0f40c5af4b5bb3cbdda10ce5428
荒井佳織 <kaoriarai@araikaorinoMacBook-Pro.local>
2023-10-28 14:06:13 +0900

    ファイル修正

---
+++ jphacks_ol2330/src/pages/result.tsx
@@ -0,0 +1,14 @@
+export default function Result() {
+    return (
+      <main className="flex min-h-screen flex-col items-center justify-between p-24">
+        <header>
+          <h1>ほんびより</h1>
+        </header>
+        <div class="content">図書館の分類からランダムに本のジャンルをお勧めします。</div>
+        <div class="btn_con">
+          <button class="random_btn" type="button">えらぶ</button>
+        </div>
+      </main>
+    )
+  }
+  

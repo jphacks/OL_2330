8501af9e0d22d137d298af632d0e298b3ce09f41
agemakimakimaki <akinari1504@gmail.com>
2023-10-28 18:34:19 +0900

    random.tsxをuseeffectとusestateを用いたものに変更。resultに変数としてindexのreturnを扱えるようにした

--- jphacks_ol2330/src/pages/result.tsx
+++ jphacks_ol2330/src/pages/result.tsx
@@ -1,15 +1,16 @@
 import Header from './components/header.tsx';
 import React from 'react';
-import { useState } from 'react';
 import IndexPage from './random';
 
 export default function Result() {
-    return (
-      <main className="flex min-h-screen flex-col items-center justify-between p-24">
-        <Header />
-        <div class="content">お勧め</div>
-        <div>IndexPage</div>
-        <IndexPage />
-      </main>
-    )
-  }
+  const MyIndexPage = IndexPage; // IndexPage を変数に代入
+  
+  return (
+    <main className="flex min-h-screen flex-col items-center justify-between p-24">
+      <Header />
+      <div class="content">お勧め</div>
+      <div>IndexPage</div> 
+      <MyIndexPage /> {/* IndexPage コンポーネントをレンダリング */}
+    </main>
+  )
+}
